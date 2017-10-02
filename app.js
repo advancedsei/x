6955f48d93eb1c46daa3a29bd75380ab341797a3
Unknown <vamsee.asei@gmail.com>
2017-10-01 20:27:14 -0400

    updated web project

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var compression = require('compression');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
 // var cookieParser = require('cookie-parser');
 // var bodyParser = require('body-parser');
 // var flash = require('connect-flash');
@@ -60,7 +60,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public'),{maxAge:864000000}));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cookieParser());
